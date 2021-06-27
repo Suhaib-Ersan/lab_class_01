05941336f1a43fdd3880923d54860d5cce4ecb12
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-06-27 17:24:48 +0300

    got upto hornedbeasts

---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+    render() {
+        return (
+          <h1>Author: Suhaib Ersan</h1>
+        )
+    }
+}
+
+export default HornedBeasts;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts'
+
+class Main extends React.Component {
+    render() {
+        return (
+          <HornedBeasts />
+          <HornedBeasts />
+        )
+    }
+}
+
+export default Main;
